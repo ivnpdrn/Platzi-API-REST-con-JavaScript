b518b8978220a8c42fee57a3d41ca7d551b3d1bd
ivnpdrn <122062585+ivnpdrn@users.noreply.github.com>
2023-08-23 23:27:04 -0400

    Favorites & Random

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // JavaScript source code
 
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_ZnPMS1KWlOzWqHjx9Vk4HDU83GG3s8oooyjD7JdbbXY2EcQrtR4LWZyd4K7itAfl';
 
 const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_ZnPMS1KWlOzWqHjx9Vk4HDU83GG3s8oooyjD7JdbbXY2EcQrtR4LWZyd4K7itAfl';
 
-        // limit= 3 images & page=2 - mostrar la segunda page
+       
 
 async function loadRandomMichis() {
     const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
-
+    console.log('Random')
     console.log(data)
 
     const img1 = document.getElementById('img1');
@@ -23,8 +23,11 @@ async function loadRandomMichis() {
 async function loadFavoriteMichis() {
     const res = await fetch(API_URL_FAVOURITES);
     const data = await res.json();
-
+    console.log('Favoritos')
     console.log(data)
+
+
+
 
 }
 
